8a502150eb7de73a69bc29aba16e1818d900dedd
mortonbits <82731659+mortonbits@users.noreply.github.com>
2021-06-21 21:38:32 +0200

    make onboard val command to respect swarm parameters

--- ol/onboard/src/commands.rs
+++ ol/onboard/src/commands.rs
@@ -16,6 +16,7 @@ use self::{
 use abscissa_core::{Command, Configurable, Help, Options, Runnable};
 use std::path::PathBuf;
 use ol_types::config::AppCfg;
+use crate::entrypoint;
 
 
 /// MinerApp Configuration Filename
@@ -61,7 +62,16 @@ impl Configurable<AppCfg> for WizCmd {
         // Check if the config file exists, and if it does not, ignore it.
         // If you'd like for a missing configuration file to be a hard error
         // instead, always return `Some(CONFIG_FILE)` here.
-        None
+
+        let mut config_path = entrypoint::get_node_home();
+
+        config_path.push(CONFIG_FILE);
+        if config_path.exists() {
+            println!("initializing from config file: {:?}", config_path);
+            Some(config_path)
+        } else {
+            None
+        }
     }
 
     // /// Apply changes to the config after it's been loaded, e.g. overriding
